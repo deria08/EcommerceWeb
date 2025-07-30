368c89dce7983866190142944edf0fa1eefe586f
deria10 <dev.deria@gmail.com>
2025-07-30 12:28:32 +0700

    first commit

--- config/db.js
+++ config/db.js
@@ -1,25 +1,30 @@
 import mongoose from "mongoose";
 
-let cached = global.mongoose;
+let cached = global.mongoose
 
 if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
+    cached = global.mongoose = {conn: null, promise: null}
 }
 
 async function connectDB() {
+
     if (cached.conn) {
-        return cached.conn;
+        return cached.conn
     }
 
     if (!cached.promise) {
-        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        }).then((mongoose) => mongoose);
+        const opts = {
+            bufferCommands: false
+        }
+
+        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`,opts).then( mongoose => {
+            return mongoose
+        })
     }
 
-    cached.conn = await cached.promise;
-    return cached.conn;
+    cached.conn = await cached.promise
+    return cached.conn
+        
 }
 
-export default connectDB;
+export default connectDB
