66be5819a1f638e1935b5c26be36a7ac05dad175
deria10 <dev.deria@gmail.com>
2025-07-30 12:17:07 +0700

    first commit

--- config/db.js
+++ config/db.js
@@ -14,7 +14,7 @@ async function connectDB() {
 
     if (!cached.promise) {
         const opts = {
-            BufferCommands: false
+            bufferCommands: false
         }
 
         cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`,opts).then( mongoose => {
